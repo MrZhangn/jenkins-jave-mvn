4d8fceb96e71ad715ad716911b670952cbaac3a4
MrZhangn <51556254+MrZhangn@users.noreply.github.com>
2022-01-06 13:30:12 +0800

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        while(true){
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello World!!");
+        }
     }
 }
